70e2d441dc34dcee5330b25a65e0d946dc015975
Deodatuss <serbgan03@gmail.com>
2024-05-13 16:54:15 +0300

    fix config reader

--- thesis/config_reader.py
+++ thesis/config_reader.py
@@ -1,11 +1,17 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import SecretStr
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class Settings(BaseSettings):
     bot_token: SecretStr
 
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(
+        env_file="/thesis/.env",
+        env_file_encoding="utf-8",
+    )
 
 
 config = Settings()
