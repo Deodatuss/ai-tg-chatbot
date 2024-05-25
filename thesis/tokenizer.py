7f8f721205e15b314dffd477ac6347a9c5df978a
Deodatuss <serbgan03@gmail.com>
2024-05-25 18:44:45 +0300

    implement tokenizer handlers

--- thesis/tokenizer.py
+++ thesis/tokenizer.py
@@ -2,7 +2,7 @@ import logging
 
 
 from bpemb import BPEmb
-from aiogram.types import Message
+from aiogram.types import Message, User
 from aiogram.fsm.context import FSMContext
 
 
@@ -12,16 +12,16 @@ bpemb_en = BPEmb(lang="en", vs=10000)
 bpemb_uk = BPEmb(lang="uk", vs=10000)
 
 
-async def _set_tokens(message: Message, state: FSMContext, tokens: int):
+async def _set_tokens(user: User, state: FSMContext, tokens: int):
     """
     Setter of tokens for some user in a state and in database.
     """
-    state.update_data(my_tokens=tokens)
-    database.set_tokens_by_id(message.from_user.id, tokens)
+    await state.update_data(my_tokens=tokens)
+    database.set_tokens_by_id(user.id, tokens)
     return None
 
 
-async def get_tokens(message: Message, state: FSMContext):
+async def get_tokens(state: FSMContext):
     """
     Getter of tokens for some user from state.
     """
@@ -29,25 +29,27 @@ async def get_tokens(message: Message, state: FSMContext):
     return user_data["my_tokens"]
 
 
-async def is_enough_tokens(message: Message, state: FSMContext, tokens_to_take: int):
+async def is_enough_tokens(state: FSMContext, tokens_to_take: int):
     """
     Input: user id and number of tokens to take away.
     If available tokens are less then required, returns false.
     """
-    if await get_tokens(message, state) >= tokens_to_take:
+    if await get_tokens(state) >= tokens_to_take:
         return True
     return False
 
 
-async def update_tokens(message: Message, state: FSMContext, tokens_to_take: int):
+async def update_tokens(user: User, state: FSMContext, tokens_to_change: int):
     """
     Input: user id to be used as a key, and number of tokens to update
     sets tokens to be old value minus tokens used.
+    Positive token values are added, e.g. from buying.
+    Negative token values are taken from balance.
     """
-    tokens = await get_tokens(message, state)
-    new_tokens_count = tokens - tokens_to_take
+    tokens = await get_tokens(state)
+    new_tokens_count = tokens + tokens_to_change
 
-    await _set_tokens(message, state, new_tokens_count)
+    await _set_tokens(user, state, new_tokens_count)
 
     return None
 
@@ -68,5 +70,7 @@ async def count_tokenize_message(message: Message, state: FSMContext) -> int:
     if lang == "ua":
         tokens: list = bpemb_uk.encode(text)
         return len(tokens)
+
+    message.answer("something went wrong with model language check")
 
     return 0
--- thesis/tokens_router.py
+++ thesis/tokens_router.py
@@ -1,28 +1,55 @@
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.filters import Command
 from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
+from aiogram.types.inline_keyboard_button import InlineKeyboardButton
+from aiogram.types.inline_keyboard_markup import InlineKeyboardMarkup
+from aiogram.types.callback_query import CallbackQuery
 
-from state_class import GenerationStages
 
 import tokenizer
+from state_class import GenerationStages
+
 
 router = Router()
 
 
-@router.message(Command("my_tokens"))
+@router.message(Command("my_tokens"), GenerationStages.setting_parameters)
 async def tokens_on_account(message: Message, state: FSMContext):
     """
     Output: message with how much tokens left on account
     Autosends after every generation
     """
-    pass
+    user_data = await state.get_data()
+    tokens = user_data["my_tokens"]
+    await message.answer(f"You have {tokens} tokens")
 
 
-@router.message(Command("buy_tokens"))
+@router.message(Command("buy_tokens"), GenerationStages.setting_parameters)
 async def buy_tokens(message: Message, state: FSMContext):
     """
     Uses inline buttons to choose how much tokens to buy.
     Currently buttons are stubs for payment, and directly add tokens instead
     """
-    pass
+    kb = [
+        [
+            InlineKeyboardButton(text="10 tokens – 0.1 $", callback_data="tokens-10"),
+            InlineKeyboardButton(text="100 tokens – 1 $", callback_data="tokens-100"),
+            InlineKeyboardButton(text="1000 tokens – 8 $", callback_data="tokens-1000"),
+        ],
+    ]
+    ikb = InlineKeyboardMarkup(inline_keyboard=kb)
+    await message.answer(
+        "Choose how much tokens to get",
+        reply_markup=ikb,
+    )
+
+
+@router.callback_query(F.data.startswith("tokens-"))
+async def recieve_tokens(callback: CallbackQuery, state: FSMContext):
+    data: str = callback.data
+    tokens = int(data.split("-")[1])
+    await callback.message.edit_text(
+        f"You just recieved {tokens} tokens. " + "check your balance with /my_tokens"
+    )
+    await tokenizer.update_tokens(callback.from_user, state, tokens)
